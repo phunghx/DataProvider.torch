f2beb9aca6ecd898223621fedd12ac9fcb3da96b
phunghx <phunghx@gmail.com>
2017-01-03 16:49:37 +0900

    transform

--- init.lua
+++ init.lua
@@ -3,5 +3,5 @@ DataProvider = {}
 torch.include('DataProvider', 'Container.lua')
 torch.include('DataProvider', 'LMDBProvider.lua')
 torch.include('DataProvider', 'FileSearcher.lua')
-
+torch.include('DataProvider','transforms.lua')
 return DataProvider
